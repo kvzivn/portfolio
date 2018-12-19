7765bba84765c629987f524133e343ad1bea4e37
kvzivn <hello@kevinivan.com>
2018-12-19 02:33:57 -0500

    updatez

--- js/main.js
+++ js/main.js
@@ -1,12 +1,60 @@
-const links = document.querySelectorAll('.js-link')
+(function () {
+    if (document.body.classList.contains('has-animations')) {
 
-document.addEventListener('DOMContentLoaded', () => document.body.classList.remove('fade'))
+        const sr = window.sr = ScrollReveal()
 
-links.forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        document.body.classList.add('fade');
+        sr.reveal('.navigation', {
+            delay: 800,
+            duration: 1200,
+            distance: '40px',
+            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            origin: 'top'
+        })
 
-        setTimeout(() => window.location = e.target.href, 2000)
-    })
-})
+        sr.reveal('.about-text', {
+            delay: 1200,
+            duration: 1200,
+            distance: '60px',
+            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            origin: 'bottom',
+            interval: 150
+        })
+
+        sr.reveal('.browser', {
+            delay: 1600,
+            duration: 1600,
+            distance: '200px',
+            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            origin: 'bottom',
+            interval: 150
+        })
+
+        sr.reveal('.hero-left-decoration', {
+            duration: 1000,
+            distance: '40px',
+            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            origin: 'bottom'
+        })
+
+        sr.reveal('.clients li', {
+            delay: 300,
+            duration: 1000,
+            rotate: {
+                y: 50
+            },
+            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            interval: 150
+        })
+
+        sr.reveal('.feature, .tabs-links li, .testimonial, .pricing-table, .pricing-faqs, .cta-inner', {
+            duration: 600,
+            distance: '40px',
+            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            interval: 100,
+            origin: 'bottom',
+            viewFactor: 0.2
+        })
+    }
+}())
+
+
