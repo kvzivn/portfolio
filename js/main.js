728e935a738d39b93dd27d5ffc7896fa73a5ba72
kvzivn <hello@kevinivan.com>
2019-01-06 16:29:49 -0500

    fix front page

--- js/main.js
+++ js/main.js
@@ -3,83 +3,30 @@
 
         const sr = window.sr = ScrollReveal()
 
-        sr.reveal('.navigation', {
-            delay: 800,
-            duration: 1200,
+        sr.reveal('.js-nav', {
+            delay: 600,
+            duration: 1000,
             distance: '40px',
             easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
             origin: 'top'
         })
 
-        sr.reveal('.bio-text', {
-            delay: 1200,
-            duration: 1200,
-            distance: '60px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom',
-            interval: 150
-        })
-
-        sr.reveal('.first', {
-            delay: 1200,
-            duration: 1200,
-            distance: '60px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom',
-            interval: 150
-        })
-
-        sr.reveal('.second', {
-            delay: 1600,
-            duration: 1200,
-            distance: '60px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom',
-            interval: 150
-        })
-
-        sr.reveal('.third', {
-            delay: 2000,
-            duration: 1200,
-            distance: '60px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom',
-            interval: 150
-        })
-
-        sr.reveal('.browser', {
-            delay: 1600,
-            duration: 1600,
-            distance: '200px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom',
-            interval: 150
-        })
-
-        sr.reveal('.hero-left-decoration', {
+        sr.reveal('.js-bio', {
+            delay: 1000,
             duration: 1000,
-            distance: '40px',
+            distance: '60px',
             easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom'
-        })
-
-        sr.reveal('.clients li', {
-            delay: 300,
-            duration: 1000,
-            rotate: {
-                y: 50
-            },
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            origin: 'bottom',
             interval: 150
         })
 
-        sr.reveal('.feature, .tabs-links li, .testimonial, .pricing-table, .pricing-faqs, .cta-inner', {
-            duration: 600,
-            distance: '40px',
+        sr.reveal('.js-projectFirst', {
+            delay: 1200,
+            duration: 1000,
+            distance: '60px',
             easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            interval: 100,
             origin: 'bottom',
-            viewFactor: 0.2
+            interval: 150
         })
     }
 }())
