e454a227b748e9bef06631a2a4a98fbdec2b5b24
kvzivn <hello@kevinivan.com>
2019-01-28 21:59:37 -0500

    update projects

--- js/main.js
+++ js/main.js
@@ -2,74 +2,76 @@
     const workLink = document.querySelector('.js-workLink')
     const aboutLink = document.querySelector('.js-aboutLink')
     const mainContent = document.querySelector('.js-content')
+    const nav = document.querySelector('.js-nav')
 
     let workPage = `
         <p class="bio-text js-bio">
-            I’m a UX designer that loves to code.<br>Currently based in Toronto, CA.
+                I’m a UX designer that loves to code.<br>Currently based in Toronto, CA.
         </p>
 
-        <section class="project project--gg js-projectFirst">
-            <a class="project-link" href="/gg">
-                <img class="project-logo" src="/img/gg/gg_logo.png" alt="grocery gateway">
-            </a>
-        </section>
-
-        <section class="project project--spinnup">
+        <section class="project project--spinnup js-projectFirst">
             <a class="project-link" href="/spinnup.html">
                 <img class="project-logo" src="/img/spinnup/spinnup_logo.png" alt="spinnup">
             </a>
         </section>
 
-        <section class="project project--polar">
+        <section class="project project--polar js-projectSecond">
             <a class="project-link" href="/polar.html">
                 <img class="project-logo project-logo--polar" src="/img/polar/polar_logo.png" alt="polar">
+            </a>
+        </section>
+
+        <section class="project project--gg js-projectThird">
+            <a class="project-link" href="/gg">
+                <img class="project-logo project-logo--gg" src="/img/gg/gg_logo.png" alt="grocery gateway">
             </a>
         </section>
     `
 
     let aboutPage = `
-            <section class="about about-text">
-                <img class="about-img" src="/img/kevin.jpg" alt="Kevin Ivan">
-                <div class="column">
-                    <p class="about-paragraph">
-                        The reason why I love design, is because right now is an incredible time to be a designer. Many of the products that we use
-                        and depend on are products of design.
-                    </p>
-                    <p class="about-paragraph">
-                        Tinder bio. Biohacker. I'm currently in the transition between front-end development and UX design. Not because
-                    </p>
-                    <p class="about-paragraph">
-                        Tell me something about you (your background, your personality, your sense of humor) that I won’t be able to find on Linkedin.
-                        Or in another designer.
-                    </p>
-                    <p class="about-paragraph first">
-                        I love to get inside the mind of people, discover their motivations, and use that information to design pleasant and accessible
-                        experiences.
-                    </p>
-                    <p class="about-paragraph third">
-                        I can confidently lead ideation sessions, conduct user interviews, identify crucial insights, and develop everything from
-                        low-fidelity prototypes to production quality code.
-                    </p>
-                </div>
-            </section>
+        <section class="about about-text">
+            <img class="about-img" src="/img/kevin.jpg" alt="Kevin Ivan">
+            <div class="column">
+                <p class="about-paragraph first">
+                    I love to get inside the mind of people, discover their motivations, and use that knowledge to design pleasant and accessible
+                    experiences.
+                </p>
+                <p class="about-paragraph first">
+                        I'm also into really cool stuff like biohacking and yoga.
+                </p>
+                <p class="about-paragraph third">
+                    I can confidently lead ideation sessions, conduct user research, and develop everything from
+                    low-fidelity prototypes to production quality code.
+                </p>
+            </div>
+        </section>
     `
 
     let routes = {
-        '/': workPage,
+        '/index.html': workPage,
         '/about.html': aboutPage
+    }
+
+    if (window.location.pathname === '/about.html') {
+        nav.classList.add('about-page')
     }
 
     if (workLink) {
         workLink.addEventListener('click', (e) => {
             e.preventDefault()
-            onNavItemClick('/')
+            onNavItemClick('/index.html')
+
+            if (nav.classList.contains('about-page')) {
+                nav.classList.remove('about-page')
+            }
         })
     }
 
     if (aboutLink) {
-            aboutLink.addEventListener('click', (e) => {
+        aboutLink.addEventListener('click', (e) => {
             e.preventDefault()
             onNavItemClick('/about.html')
+            nav.classList.add('about-page')
         })
     }
 
@@ -91,98 +93,112 @@
 
         const sr = window.sr = ScrollReveal()
 
-        sr.reveal('.js-heading', {
-            delay: 800,
-            duration: 1000,
-            distance: '40px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom',
-            interval: 150
-        })
+        // sr.reveal('.js-heading', {
+        //     delay: 800,
+        //     duration: 1000,
+        //     distance: '40px',
+        //     easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        //     origin: 'bottom',
+        //     interval: 150
+        // })
 
-        sr.reveal('.js-subtitle', {
-            delay: 1000,
-            duration: 1000,
-            distance: '40px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom',
-            interval: 150
-        })
+        // sr.reveal('.js-subtitle', {
+        //     delay: 1000,
+        //     duration: 1000,
+        //     distance: '40px',
+        //     easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        //     origin: 'bottom',
+        //     interval: 150
+        // })
 
-        sr.reveal('.color', {
-            delay: 1400,
-            duration: 600,
-            distance: '20px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            interval: 100,
-            origin: 'bottom',
-            viewFactor: 0.2
-        })
+        // sr.reveal('.color', {
+        //     delay: 1400,
+        //     duration: 600,
+        //     distance: '20px',
+        //     easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        //     interval: 100,
+        //     origin: 'bottom',
+        //     viewFactor: 0.2
+        // })
 
-        sr.reveal('.intro-infoItem', {
-            delay: 2000,
-            duration: 1400,
-            distance: '100px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            interval: 150,
-            origin: 'left'
-        })
+        // sr.reveal('.intro-infoItem', {
+        //     delay: 2000,
+        //     duration: 1400,
+        //     distance: '100px',
+        //     easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        //     interval: 150,
+        //     origin: 'left'
+        // })
 
-        sr.reveal('.js-textHeading', {
-            delay: 600,
-            duration: 800,
-            distance: '60px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom',
-            interval: 150
-        })
+        // sr.reveal('.js-textHeading', {
+        //     delay: 600,
+        //     duration: 800,
+        //     distance: '60px',
+        //     easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        //     origin: 'bottom',
+        //     interval: 150
+        // })
 
-        sr.reveal('.js-paragraph', {
-            delay: 800,
-            duration: 800,
-            distance: '60px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            interval: 400,
-            origin: 'bottom',
-            interval: 150
-        })
+        // sr.reveal('.js-paragraph', {
+        //     delay: 800,
+        //     duration: 800,
+        //     distance: '60px',
+        //     easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        //     interval: 400,
+        //     origin: 'bottom',
+        //     interval: 150
+        // })
 
-        sr.reveal('.js-img', {
-            delay: 1200,
-            duration: 1400,
-            distance: '60px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom',
-            interval: 150
-        })
+        // sr.reveal('.js-img', {
+        //     delay: 1200,
+        //     duration: 1400,
+        //     distance: '60px',
+        //     easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        //     origin: 'bottom',
+        //     interval: 150
+        // })
 
 
 
-        sr.reveal('.js-nav', {
-            delay: 600,
-            duration: 1000,
-            distance: '40px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'top'
-        })
+        // sr.reveal('.js-nav', {
+        //     delay: 600,
+        //     duration: 1000,
+        //     distance: '40px',
+        //     easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        //     origin: 'top'
+        // })
 
-        sr.reveal('.js-bio', {
-            delay: 1000,
-            duration: 1000,
-            distance: '60px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom',
-            interval: 150
-        })
+        // sr.reveal('.js-bio', {
+        //     delay: 1000,
+        //     duration: 1000,
+        //     distance: '60px',
+        //     easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        //     origin: 'bottom',
+        //     interval: 150
+        // })
 
-        sr.reveal('.js-projectFirst', {
-            delay: 1200,
-            duration: 1000,
-            distance: '60px',
-            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-            origin: 'bottom',
-            interval: 150
-        })
+        // sr.reveal('.js-projectFirst', {
+        //     delay: 1200,
+        //     duration: 1000,
+        //     distance: '60px',
+        //     easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        //     origin: 'bottom',
+        //     interval: 150
+        // })
+
+        // sr.reveal('.js-projectSecond', {
+        //     delay: 200,
+        //     duration: 1200,
+        //     distance: '100px',
+        //     origin: 'bottom'
+        // })
+
+        // sr.reveal('.js-projectThird', {
+        //     delay: 200,
+        //     duration: 1200,
+        //     distance: '100px',
+        //     origin: 'bottom'
+        // })
     }
 
 }())
